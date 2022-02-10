de22dd2658a391514427792af63c84188678ea6c
Jabir Minjibir <jabir.minjibir@gmail.com>
2022-02-10 16:03:12 -0500

    Merged the two seperate repos.

--- src/main/scala/ds/Jist.scala
+++ src/main/scala/ds/Jist.scala
@@ -1,147 +1,145 @@
 package ds
 
-	object Jist {
+object Jist {
 
-		def apply[A](a: A*): Jist[A] =
-			if (a.isEmpty) EmptyJist
-			else Cons(a.head, apply(a.tail: _*))
+  def apply[A](a: A*): Jist[A] =
+    if (a.isEmpty) EmptyJist
+    else Cons(a.head, apply(a.tail: _*))
 
-		def sum(list: Jist[Int]): Int = {
+  def tail[A](list: Jist[A]): Jist[A] = list match {
+    case EmptyJist     => EmptyJist
+    case Cons(_, tail) => tail
+  }
 
-			@annotation.tailrec
-			def loop(xs: Jist[Int], acc: Int): Int = xs match {
-				case EmptyJist => acc
-				case Cons(h, t) => loop(t, acc + h)
-			}
+  def setHead[A](list: Jist[A], head: A): Jist[A] = list match {
+    case EmptyJist   => Cons(head, EmptyJist)
+    case Cons(_, xs) => Cons(head, xs)
+  }
 
-			loop(list, 0)
+  def sum(jist: Jist[Int]): Int =
+    foldLeft(jist, 0)(_ + _)
 
-		}
+  def product(jist: Jist[Double]): Double =
+    foldLeft(jist, 1.0)(_ * _)
 
-		def product(list: Jist[Int]): Int = {
-
-			@annotation.tailrec
-			def loop(lst: Jist[Int], acc: Int): Int = lst match {
-				case EmptyJist => acc
-				case Cons(h, t) => loop(t, acc * h)
-			}
-
-			loop(list, 1)
-
-		}
-
-		def tail[A](list: Jist[A]): Jist[A] = list match {
-			case EmptyJist => EmptyJist
-			case Cons(_, tail) => tail
-		}
-
-		def setHead[A](list: Jist[A], head: A): Jist[A] = list match {
-			case EmptyJist => Cons(head, EmptyJist)
-			case Cons(_, xs) => Cons(head, xs)
-		}
-
-		def append[A](a: A, list: Jist[A]): Jist[A] = list match {
-			case EmptyJist => Cons(a, EmptyJist)
-			case Cons(x, xs) => Cons(x, append(a, xs))
-		}
-
-		def concat[A](a: Jist[A], b: Jist[A]): Jist[A] = (a, b) match {
-			case (EmptyJist, EmptyJist) => EmptyJist
-			case (Cons(_, _), EmptyJist) => a
-			case (EmptyJist, Cons(_, _)) => b
-			case (Cons(x, xs), Cons(_, ys)) => Cons(x, concat(xs, b))
-		}
-
-		@annotation.tailrec
-		def drop[A](l: Jist[A], n: Int): Jist[A] = {
-			if (l == EmptyJist || n <= 0) l
-			else drop(tail(l), n - 1)
-		}
-
-//		@annotation.tailrec
-		def dropWhile[A](l: Jist[A], f: A => Boolean): Jist[A] = {
-
-			def loop(jist: Jist[A], f: A => Boolean, state: Jist[A] = EmptyJist): Jist[A] =
-				jist match {
-					case EmptyJist => state
-					case Cons(x, xs) if f(x) =>
-						loop(xs, f, jist)
-				}
-
-				loop(l, f, EmptyJist)
-			}
-
-		def foldLeft[A, B](jist: Jist[A], b: B)(f: (A, B) => B): B = jist match {
-			case EmptyJist => b
-			case Cons(h, t) => foldLeft(t, f(h, b))(f)
-		}
-
+  def append[A](a: A, list: Jist[A]): Jist[A] =
 		/*
-		Not everything works out so nicely. Implement a function, init, that returns a Jist
-		consisting of all but the last element of a Jist. So, given Jist(1,2,3,4), init will
-		return Jist(1,2,3). Why can’t this function be implemented in constant time like
-		tail?
+		foldLeft(list, list)(Cons(append(a)))
 		*/
-		def init[Int](list: Jist[Int]): Jist[Int] = list match {
-			case EmptyJist => EmptyJist
-			case Cons(_, EmptyJist) => EmptyJist
-			case Cons(x, Cons(_, EmptyJist)) => Cons(x, EmptyJist)
-			case Cons(x, Cons(y, ys)) => Cons(x, Cons(y, init(ys)))
+	list match {
+    case EmptyJist   => Cons(a, EmptyJist)
+    case Cons(x, xs) => Cons(x, append(a, xs))
+  }
+
+  def concat[A](a: Jist[A], b: Jist[A]): Jist[A] = (a, b) match {
+    case (EmptyJist, EmptyJist)    => EmptyJist
+    case (Cons(_, _), EmptyJist)   => a
+    case (EmptyJist, Cons(_, _))   => b
+    case (Cons(x, xs), Cons(_, _)) => Cons(x, concat(xs, b))
+  }
+
+  @annotation.tailrec
+  def drop[A](l: Jist[A], n: Int): Jist[A] = {
+    if (l == EmptyJist || n <= 0) l
+    else drop(tail(l), n - 1)
+  }
+
+  @annotation.tailrec
+  def dropWhile[A](l: Jist[A])(f: A => Boolean): Jist[A] = l match {
+    case Cons(h, t) if f(h) => dropWhile(t)(f)
+    case _                  => l
+  }
+
+  def init[Int](list: Jist[Int]): Jist[Int] = list match {
+    case EmptyJist                   => EmptyJist
+    case Cons(_, EmptyJist)          => EmptyJist
+    case Cons(x, Cons(_, EmptyJist)) => Cons(x, EmptyJist)
+    case Cons(x, Cons(y, ys))        => Cons(x, Cons(y, init(ys)))
+  }
+
+  def length[A](jist: Jist[A]): Int =
+    foldRight(jist, 0)((_, len) => len + 1)
+
+  @annotation.tailrec
+  def foldLeft[A, B](jist: Jist[A], b: B)(f: (A, B) => B): B =
+    jist match {
+      case EmptyJist  => b
+      case Cons(h, t) => foldLeft(t, f(h, b))(f)
+    }
+
+  def foldRight[A, B](jist: Jist[A], b: B)(f: (A, B) => B): B =
+    foldLeft(jist, b)(f)
+
+  def reverse[A](jist: Jist[A]): Jist[A] =
+    foldLeft(jist, EmptyJist: Jist[A])(Cons(_, _))
+
+  def map[A, B](as: Jist[A])(f: A => B): Jist[B] = {
+
+    @annotation.tailrec
+    def loop(a: Jist[A], d: Jist[B])(f: A => B): Jist[B] =
+      a match {
+        case EmptyJist  => d
+        case Cons(h, t) => loop(t, append(f(h), d))(f)
+      }
+
+    loop(as, EmptyJist)(f)
+  }
+
+  def filter[A](as: Jist[A])(f: A => Boolean): Jist[A] = {
+
+    @annotation.tailrec
+    def loop(a: Jist[A], b: Jist[A])(f: A => Boolean): Jist[A] = a match {
+      case EmptyJist => b
+      case Cons(h, t) =>
+        val acc: Jist[A] = if (f(h)) append(h, b) else b
+        loop(t, acc)(f)
+    }
+
+    loop(as, EmptyJist)(f)
+  }
+
+  def flatMap[A, B](as: Jist[A])(f: A => Jist[B]): Jist[B] = {
+
+    @annotation.tailrec
+    def loop(a: Jist[A], b: Jist[B])(f: A => Jist[B]): Jist[B] =
+      a match {
+        case EmptyJist  => b
+        case Cons(h, t) => loop(t, concat(b, f(h)))(f)
+      }
+
+    loop(as, EmptyJist)(f)
+  }
+
+  // def flatFilter[A](as: Jist[A])(f: A => Boolean): Jist[A] =
+  // 	flatMap(as)(f)
+
+  def zipWith[A](a: Jist[A], b: Jist[A])(f: (A, A) => A): Jist[A] = {
+    def loop(x: Jist[A], y: Jist[A], z: Jist[A])(f: (A, A) => A): Jist[A] =
+      (x, y) match {
+        case (EmptyJist, EmptyJist)  => z
+        case (Cons(_, _), EmptyJist) => concat(z, x)
+        case (EmptyJist, Cons(_, _)) => concat(z, y)
+        case (Cons(ah, at), Cons(bh, bt)) =>
+          loop(at, bt, append(f(ah, bh), z))(f)
+      }
+
+    loop(a, b, EmptyJist)(f)
+  }
+
+  def hasSubsequence[A](sup: Jist[A], sub: Jist[A]): Boolean =
+		sup match {
+			case EmptyJist => false
+			case Cons(h, t) =>
+				val supLen = Jist.length(sup)
+				val subLen = Jist.length(sub)
+				val lenDif = supLen - subLen
+
+				val first = Jist.drop(sup, lenDif)
+				val second = Jist.dropLastN(sup, lenDif)
+
+				(sub == first) || (sub == second)
 		}
-
-		def map[A, B](as: Jist[A])(f: A => B): Jist[B] = {
-			
-			@annotation.tailrec
-			def loop(a: Jist[A], d: Jist[B])(f: A => B): Jist[B] =
-				a match {
-					case EmptyJist => d
-					case Cons(h, t) => loop(t, append(f(h), d))(f)
-				}
-
-				loop(as, EmptyJist)(f)
-		}
-
-		def filter[A](as: Jist[A])(f: A => Boolean): Jist[A] = {
-
-			@annotation.tailrec
-			def loop(a: Jist[A], b: Jist[A])(f: A => Boolean): Jist[A] = a match {
-				case EmptyJist => b
-				case Cons(h, t) =>
-					val acc: Jist[A] = if(f(h)) append(h, b) else b
-					loop(t, acc)(f)
-			}
-
-			loop(as, EmptyJist)(f)
-		}
-
-		def flatMap[A, B](as: Jist[A])(f: A => Jist[B]): Jist[B] = {
-
-			@annotation.tailrec
-			def loop(a: Jist[A], b: Jist[B])(f: A => Jist[B]): Jist[B] =
-				a match {
-					case EmptyJist => b
-					case Cons(h, t) => loop(t, concat(b, f(h)))(f)
-				}
-
-			loop(as, EmptyJist)(f)
-		}
-
-		// def flatFilter[A](as: Jist[A])(f: A => Boolean): Jist[A] =
-		// 	flatMap(as)(f)
-
-		def zipWith[A](a: Jist[A], b: Jist[A])(f: (A, A) => A): Jist[A] = {
-			def loop(x: Jist[A], y: Jist[A], z: Jist[A])(f: (A, A) => A): Jist[A] =
-				(x, y) match {
-					case (EmptyJist, EmptyJist) => z
-					case (Cons(_, _), EmptyJist) => concat(z, x)
-					case (EmptyJist, Cons(_, _)) => concat(z, y)
-					case (Cons(ah, at), Cons(bh, bt)) =>
-						loop(at, bt, append(f(ah, bh), z))(f)
-				}
-
-			loop(a, b, EmptyJist)(f)
-		}
-	}
+}
 
 sealed trait Jist[+A]
 case object EmptyJist extends Jist[Nothing]
--- src/test/scala/ds/JistSpec.scala
+++ src/test/scala/ds/JistSpec.scala
@@ -5,12 +5,6 @@ import scala.collection.View
 
 class JistSpec extends AnyWordSpec {
 
-  // val jistInt: Jist[Int]    = Jist(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
-  // val jistStr: Jist[String] = Jist("a", "b", "c", "d", "e", "f")
-
-  // val str0jist: Jist[String] = Jist("a", "b", "c")
-  // val str1jist: Jist[String] = Jist("d", "e", "f")
-
   "tail function" when {
     "called on an EmptyJist" must {
       "return an EmptyJist" in {
@@ -119,15 +113,244 @@ class JistSpec extends AnyWordSpec {
       }
     }
 
-    "when both Jist are non empty" should {
+    "both Jist are non empty" should {
       "return the two merged into one Jist" in {
-        val str0jist  = Jist("a", "b", "c")
-        val str1jist  = Jist("d", "e", "f")
-        val actual    = Jist("a", "b", "c", "d", "e", "f")
+        val str0jist = Jist("a", "b", "c")
+        val str1jist = Jist("d", "e", "f")
+        val actual = Jist("a", "b", "c", "d", "e", "f")
 
-        val expected  = Jist.concat(str0jist, str1jist)
+        val expected = Jist.concat(str0jist, str1jist)
 
         assert(expected == actual)
+      }
+    }
+  }
+
+  "drop function" when {
+    "given an empty Jist" must {
+      "return an empty Jist" in {
+        val decker: Jist[String] = Jist()
+        val result: Jist[String] = Jist.drop(decker, 1)
+
+        assert(result.equals(EmptyJist))
+      }
+    }
+
+    "given a Jist with single element" must {
+      "return empty Jist" in {
+        val decker: Jist[Int] = Cons(1, EmptyJist)
+        val result: Jist[Int] = Jist.drop(decker, 1)
+
+        assert(result.equals(EmptyJist))
+      }
+    }
+
+    "given a Jist with multiple elements" should {
+      "return all but the specified number" in {
+        val decker: Jist[Int] = Jist(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+        val result: Jist[Int] = Jist.drop(decker, 3)
+
+        assert(result.equals(Jist(4, 5, 6, 7, 8, 9, 0)))
+      }
+    }
+  }
+
+  "dropWhile function" when {
+    "given an empty Jist" must {
+      "return an empty Jist" in {
+        val decker: Jist[Int] = Jist()
+        val result: Jist[Int] = Jist.dropWhile(decker)(_ < 4)
+
+        assert(result.equals(EmptyJist))
+      }
+    }
+
+    "given a Jist with single element" must {
+      "return empty Jist" in {
+        val decker: Jist[String] = Jist("string")
+        val result: Jist[String] = Jist.dropWhile(decker)(_.contains("s"))
+
+        assert(result.equals(EmptyJist))
+      }
+    }
+
+    "given a Jist with multiple elements" should {
+      "return all but the specified number" in {
+        val decker: Jist[Int] = Jist(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        val result0: Jist[Int] = Jist.dropWhile(decker)(i => i < 5)
+        val result1: Jist[Int] = Jist.dropWhile(decker)(_ < 0)
+        val result2: Jist[Int] = Jist.dropWhile(decker)(_ < 9)
+
+        assert(result0 equals Jist(5, 6, 7, 8, 9))
+        assert(result1 equals decker)
+        assert(result2 equals Jist(9))
+        assert(result2 equals Cons(9, EmptyJist))
+      }
+    }
+  }
+
+  "init function" when {
+    "called with empty Jist argument" should {
+      "return an empty Jist" in {
+        val expected: Jist[String] = Jist.init(EmptyJist)
+
+        assert(expected equals EmptyJist)
+      }
+    }
+
+    "called with single element Jist" should {
+      "return empty jist" in {
+        val expected: Jist[Int] = Jist.init(Jist(1))
+
+        assert(expected equals EmptyJist)
+      }
+    }
+
+    "called with multiple element Jist" should {
+      "return empty jist" in {
+        val expected0: Jist[Int] = Jist.init(Jist(0, 1))
+        val expected1: Jist[Int] = Jist.init(Jist(0, 1, 2, 3, 4))
+
+        assert(expected0 equals Jist(0))
+        assert(expected0 equals Cons(0, EmptyJist))
+        assert(expected1 equals Jist(0, 1, 2, 3))
+      }
+    }
+
+  }
+
+  "sum function" when {
+    "given an empty Jist of integes" must {
+      "return 0" in {
+        val result = Jist.sum(Jist())
+        assert(result equals 0)
+      }
+    }
+
+    "given non empty Jist of integers" must {
+      "return their sum" in {
+        val result = Jist.sum(Jist(1, 2, 3, 4))
+        assert(result equals 10)
+      }
+    }
+  }
+
+  "product function" when {
+    "given an empty Jist of doubles" must {
+      "return 1" in {
+        val result = Jist.product(Jist())
+        assert(result equals 1d)
+      }
+    }
+
+    "given non empty Jist of integers" must {
+      "return their product" in {
+        val result = Jist.product(Jist(1, 2, 3, 4))
+        assert(result equals 24d)
+      }
+    }
+  }
+
+  "length function implementation using foldRight" when {
+    "called on Jist" should {
+      "return 0 for EmptyJist" in {
+        assert(0 equals Jist.length(EmptyJist))
+      }
+
+      "return 1 for single element Jist" in {
+        assert(1 equals Jist.length(Jist(1)))
+      }
+
+      "return 7 for single element Jist" in {
+        assert(7 equals Jist.length(Jist(0, 1, 2, 3, 4, 5, 6)))
+      }
+    }
+  }
+
+  "foldRight" when {
+    "run on any Jist" should {
+      "return 0 for EmptyJist" in {
+        val jist: Jist[Int] = EmptyJist
+        val result: Int = Jist.foldRight(jist, 0)(_ + _)
+
+        assert(result equals 0)
+      }
+
+      "return the correct sum for non empty Jist" in {
+        val jist = Jist(3)
+        assert(3 equals Jist.foldRight(jist, 0)(_ + _))
+      }
+
+      "return 15 for a Jist with sum up to 15" in {
+        val jist = Jist(1, 2, 3, 4, 5)
+        val sum = Jist.foldRight(jist, 0)(_ + _)
+        assert(sum equals 15)
+      }
+
+      "return 120 for a Jist with multiple elements" in {
+        val jist = Jist(1, 2, 3, 4, 5)
+        val prod = Jist.foldRight(jist, 1)(_ * _)
+        assert(prod equals 120)
+      }
+    }
+  }
+
+  "foldLeft" when {
+    "run on any Jist" should {
+      "return 0 for EmptyJist" in {
+        val jist: Jist[Int] = EmptyJist
+        val result: Int = Jist.foldLeft(jist, 0)(_ + _)
+
+        assert(result equals 0)
+      }
+
+      "return the correct sum for non empty Jist" in {
+        val jist = Jist(3)
+        assert(3 equals Jist.foldLeft(jist, 0)(_ + _))
+      }
+
+      "return 15 for a Jist with sum up to 15" in {
+        val jist = Jist(1, 2, 3, 4, 5)
+        val sum = Jist.foldLeft(jist, 0)(_ + _)
+        assert(sum equals 15)
+      }
+
+      "return 120 for a Jist with multiple elements" in {
+        val jist = Jist(1, 2, 3, 4, 5)
+        val prod = Jist.foldLeft(jist, 1)(_ * _)
+        assert(prod equals 120)
+      }
+    }
+  }
+
+  "reverse function" when {
+    "called upon a valid Jist" should {
+      "return the same as input when called with EmptyJist" in {
+        assert(EmptyJist equals Jist.reverse(EmptyJist))
+      }
+
+      "return a Jist with the elements swap in 2 element Jist" in {
+        val jist: Jist[String] = Cons("a", Cons("b", EmptyJist))
+        val expect = Jist("b", "a")
+        val result = Jist.reverse(jist)
+
+        assert(expect equals result)
+      }
+
+      "return a Jist with the elements swap in 3 element Jist" in {
+        val jist: Jist[String] = Cons("a", Cons("b", Cons("c", EmptyJist)))
+        val expect = Jist("c", "b", "a")
+        val result = Jist.reverse(jist)
+
+        assert(expect equals result)
+      }
+
+      "return a reversed version of the Jist given" in {
+        val jist: Jist[String] = Jist("a", "b", "c", "d", "e", "f", "g")
+        val expect = Jist("g", "f", "e", "d", "c", "b", "a")
+        val result = Jist.reverse(jist)
+
+        assert(expect equals result)
       }
     }
   }
@@ -137,7 +360,7 @@ class JistSpec extends AnyWordSpec {
       val jist = Jist(0, 1, 2, 3, 4, 5)
       val expected = Jist(1, 2, 3, 4, 5, 6)
       val actual = Jist.map(jist)(_ + 1)
-      
+
       assert(actual == expected)
     }
 
@@ -145,7 +368,7 @@ class JistSpec extends AnyWordSpec {
       val jist = Jist(0, 1, 2, 3, 4, 5)
       val expected = Jist(0, 2, 4, 6, 8, 10)
       val actual = Jist.map(jist)(_ * 2)
-      
+
       assert(actual == expected)
     }
   }
@@ -155,25 +378,25 @@ class JistSpec extends AnyWordSpec {
       val jist = Jist(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
       val expected = Jist(1, 3, 5, 7, 9)
       val actual = Jist.filter(jist)(_ % 2 > 0)
-      
+
       assert(actual == expected)
     }
-  
+
     "filter out the strings that doesn't matches the given cond" in {
       val jist = Jist("a", "b", "c", "d", "e")
       val expected = Jist("b", "c", "d", "e")
       val actual = Jist.filter(jist)(_ != "a")
-      
+
       assert(actual == expected)
     }
   }
 
   "flatMap function called with multiple Jist" should {
-    "return a single list containing all the sub-list's elems" in {      
-      val expected = Jist(1,1,2,2,3,3)
-      
-      val actual = Jist.flatMap(Jist(1,2,3))(i => Jist(i,i))
-      
+    "return a single list containing all the sub-list's elems" in {
+      val expected = Jist(1, 1, 2, 2, 3, 3)
+
+      val actual = Jist.flatMap(Jist(1, 2, 3))(i => Jist(i, i))
+
       assert(actual == expected)
     }
   }
@@ -183,10 +406,10 @@ class JistSpec extends AnyWordSpec {
       val a = Jist(1, 2, 3)
       val b = Jist(4, 5, 6)
       val expected = Jist(5, 7, 9)
-      val actual   = Jist.zipWith(a, b)(_ + _)
+      val actual = Jist.zipWith(a, b)(_ + _)
 
       assert(actual == expected)
     }
   }
-  
+
 }
