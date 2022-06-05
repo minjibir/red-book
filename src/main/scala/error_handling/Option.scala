59763ad8fe623c922e584a0d6f72b2e29aedfd64
Jabir Minjibir <jabir.minjibir@gmail.com>
2022-06-05 00:19:53 -0400

    Implemented operational functions.

--- src/main/scala/error_handling/Option.scala
+++ src/main/scala/error_handling/Option.scala
@@ -1,5 +1,29 @@
 package error_handling
 
-sealed trait Option[A]
-case object None extends Option[Nothing]
-case class Some[A]() extends Option[A]
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+			case None => None
+			case Some(a) => Some(f(a))
+	}
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+		if(this.equals(None)) None
+		else f(this.asInstanceOf[Some[A]].get)
+
+  def getOrElse[B >: A](default: => B): B = this match {
+		case None => default
+		case Some(a) => a
+	}
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+		if(this == None) ob
+		else this
+
+  def filter(f: A => Boolean): Option[A] =
+		if(this.equals(None)) None
+		else if(f(this.asInstanceOf[Some[A]].get)) this
+		else None
+}
+
+final case object None extends Option[Nothing]
+final case class Some[+A](get: A) extends Option[A]
